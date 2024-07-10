fed6431c38d889f870beb6cf5d1051f564200a1f
Pashos-2004 <Pashos-2004@yandex.ru>
2024-07-10 03:40:09 +0300

    Первая рабочая версия, добавлен запрос к корневому каталогу(можно получить загрузку процессора за последний час)

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from fastapi import BackgroundTasks, FastAPI
 import uvicorn
+import psycopg2
+import datetime
 app = FastAPI()
+
+connection = psycopg2.connect(dbname='CPU_LOAD', user='postgres', password='1111', host='127.0.0.1');
 
 @app.get("/")
 def read_root():
-    return {"<p>Привет)</p>"}
+    date=datetime.datetime.now()+datetime.timedelta(hours=-1);
+
+    date = str(date.date()) + " " + str(date.hour) + ":" + str(date.minute) + ":" + str(date.second)
+    with connection.cursor() as cursor:
+        cursor.execute(f"Begin select * from cpu_load_info where cur_data>='{date}' commit")
+        data=cursor.fetchall()
+        print(data)
+    return data
 
 uvicorn.run(
         app,
