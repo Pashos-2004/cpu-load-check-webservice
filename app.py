4a5ac6f909646804e7532f5a3c404fd5408f7123
Pashos-2004 <Pashos-2004@yandex.ru>
2024-07-10 03:43:29 +0300

    Первая рабочая версия, добавлен запрос к корневому каталогу(можно получить загрузку процессора за последний час)

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def read_root():
 
     date = str(date.date()) + " " + str(date.hour) + ":" + str(date.minute) + ":" + str(date.second)
     with connection.cursor() as cursor:
-        cursor.execute(f"\select * from cpu_load_info where cur_data>='{date}'")
+        cursor.execute(f"select * from cpu_load_info where cur_data>='{date}'")
         data=cursor.fetchall()
         print(data)
     return data
