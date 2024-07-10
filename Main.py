40e858660a9929b3b7e55f3cd8bc190d5eff9fef
Pashos-2004 <Pashos-2004@yandex.ru>
2024-07-10 03:16:16 +0300

    Оболочка 2.0

--- Main.py
+++ Main.py
@@ -4,9 +4,6 @@ import subprocess
 import multiprocessing
 import  os
 
-def bye():
-    os.close("check_CPU.py")
-
 if __name__ == '__main__':
     os.startfile("check_CPU.py")
     os.startfile('app.py')
